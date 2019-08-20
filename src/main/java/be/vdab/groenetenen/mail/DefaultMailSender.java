e0af4a5de8f901abc27900db53f8adcae3d2cad0
Matlockyholmes <matthiasheylen1@gmail.com>
2019-08-21 00:04:39 +0200

    Back in tawn

---
+++ src/main/java/be/vdab/groenetenen/mail/DefaultMailSender.java
@@ -0,0 +1,39 @@
+package be.vdab.groenetenen.mail;
+
+import be.vdab.groenetenen.domain.Offerte;
+import be.vdab.groenetenen.exceptions.KanMailNietZendenException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class DefaultMailSender implements MailSender {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final JavaMailSender sender;
+
+    public DefaultMailSender(JavaMailSender sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public void nieuweOfferte(Offerte offerte, String offertesURL) {
+        try {
+            MimeMessage message = sender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setTo(offerte.getEmailAdres());
+            helper.setSubject("Nieuwe offerte");
+            String offerteURL = offertesURL + offerte.getId();
+            helper.setText("Uw offerte heeft het nummer <strong>" + offerte.getId()+"</strong>. Je vindt de offerte <a href='" + offerteURL + "'>hier</a>.",true);
+            sender.send(message);
+        } catch (MailException | MessagingException ex){
+            logger.error("Kan mail nieuwe offerte niet versturen", ex);
+            throw new KanMailNietZendenException();
+        }
+    }
+}
--- src/main/java/be/vdab/groenetenen/services/DefaultOfferteService.java
+++ src/main/java/be/vdab/groenetenen/services/DefaultOfferteService.java
@@ -1,6 +1,7 @@
 package be.vdab.groenetenen.services;
 
 import be.vdab.groenetenen.domain.Offerte;
+import be.vdab.groenetenen.mail.MailSender;
 import be.vdab.groenetenen.repositories.OfferteRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -10,14 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
 public class DefaultOfferteService implements OfferteService{
     private final OfferteRepository repository;
+    private final MailSender mailSender;
 
-    public DefaultOfferteService(OfferteRepository repository) {
+    public DefaultOfferteService(OfferteRepository repository, MailSender mailSender) {
         this.repository = repository;
+        this.mailSender = mailSender;
     }
 
     @Override
     @Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED)
-    public void create(Offerte offerte) {
+    public void create(Offerte offerte, String offertesURL) {
         repository.save(offerte);
+        mailSender.nieuweOfferte(offerte, offertesURL);
     }
 }
